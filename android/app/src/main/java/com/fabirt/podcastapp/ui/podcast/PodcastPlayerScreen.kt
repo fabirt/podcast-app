c618a5dc695661cb5f639925c5b5ce22f3c62b70
Fabi Diartt <fandig27@gmail.com>
2021-05-02 22:54:08 -0500

    Using Palette API

--- android/app/src/main/java/com/fabirt/podcastapp/ui/podcast/PodcastPlayerScreen.kt
+++ android/app/src/main/java/com/fabirt/podcastapp/ui/podcast/PodcastPlayerScreen.kt
@@ -3,26 +3,32 @@ package com.fabirt.podcastapp.ui.podcast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.OnBackPressedDispatcher
 import androidx.compose.animation.*
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Close
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import coil.request.ImageRequest
+import com.fabirt.podcastapp.R
 import com.fabirt.podcastapp.domain.model.Episode
 import com.fabirt.podcastapp.ui.common.IconButton
 import com.fabirt.podcastapp.ui.common.ViewModelProvider
+import com.google.accompanist.coil.rememberCoilPainter
 import com.google.accompanist.insets.systemBarsPadding
 import kotlin.math.roundToInt
 
@@ -66,6 +72,23 @@ private fun Content(episode: Episode, backDispatcher: OnBackPressedDispatcher) {
         }
     }
 
+    val backgroundColor = MaterialTheme.colors.background
+    var gradientColor by remember {
+        mutableStateOf(backgroundColor)
+    }
+
+    val imageRequest = ImageRequest.Builder(LocalContext.current)
+        .data(episode.image)
+        .target {
+            podcastPlayer.calculateColorPalette(it) { color ->
+                gradientColor = color
+            }
+        }
+        .build()
+
+    val imagePainter = rememberCoilPainter(request = imageRequest)
+
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -84,46 +107,65 @@ private fun Content(episode: Episode, backDispatcher: OnBackPressedDispatcher) {
         Box(
             modifier = Modifier
                 .offset { IntOffset(0, swipeableState.offset.value.roundToInt()) }
-                .background(MaterialTheme.colors.background)
                 .fillMaxSize()
-                .systemBarsPadding()
         ) {
-            Surface(
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Column {
-                    IconButton(
-                        imageVector = Icons.Rounded.Close,
-                        contentDescription = "close"
-                    ) {
-                        podcastPlayer.showPlayerFullScreen = false
-                    }
-
-                    Column(
-                        modifier = Modifier.padding(horizontal = 24.dp)
-                    ) {
-                        PodcastImage(
-                            url = episode.image,
-                            modifier = Modifier.padding(vertical = 32.dp)
+            Surface {
+                Box(
+                    modifier = Modifier
+                        .background(
+                            Brush.verticalGradient(
+                                listOf(gradientColor, MaterialTheme.colors.background)
+                            )
                         )
-                        Text(
-                            episode.titleOriginal,
-                            style = MaterialTheme.typography.h5,
-                            color = MaterialTheme.colors.onBackground,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                        )
+                        .fillMaxSize()
+                        .systemBarsPadding()
+                ) {
+                    Column {
+                        IconButton(
+                            imageVector = Icons.Rounded.Close,
+                            contentDescription = "close"
+                        ) {
+                            podcastPlayer.showPlayerFullScreen = false
+                        }
 
-                        Text(
-                            episode.podcast.titleOriginal,
-                            style = MaterialTheme.typography.subtitle1,
-                            color = MaterialTheme.colors.onBackground,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            modifier = Modifier.graphicsLayer {
-                                alpha = 0.60f
+                        Column(
+                            modifier = Modifier.padding(horizontal = 24.dp)
+                        ) {
+
+                            Box(
+                                modifier = Modifier
+                                    .padding(vertical = 32.dp)
+                                    .clip(MaterialTheme.shapes.medium)
+                                    .aspectRatio(1f)
+                                    .background(MaterialTheme.colors.onBackground.copy(alpha = 0.08f))
+                            ) {
+                                Image(
+                                    painter = imagePainter,
+                                    contentDescription = stringResource(R.string.podcast_thumbnail),
+                                    contentScale = ContentScale.Crop,
+                                    modifier = Modifier.fillMaxSize(),
+                                )
                             }
-                        )
+
+                            Text(
+                                episode.titleOriginal,
+                                style = MaterialTheme.typography.h5,
+                                color = MaterialTheme.colors.onBackground,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                            )
+
+                            Text(
+                                episode.podcast.titleOriginal,
+                                style = MaterialTheme.typography.subtitle1,
+                                color = MaterialTheme.colors.onBackground,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                modifier = Modifier.graphicsLayer {
+                                    alpha = 0.60f
+                                }
+                            )
+                        }
                     }
                 }
             }
--- android/app/src/main/java/com/fabirt/podcastapp/ui/viewmodel/PodcastPlayerViewModel.kt
+++ android/app/src/main/java/com/fabirt/podcastapp/ui/viewmodel/PodcastPlayerViewModel.kt
@@ -1,10 +1,15 @@
 package com.fabirt.podcastapp.ui.viewmodel
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.support.v4.media.MediaBrowserCompat
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
+import androidx.palette.graphics.Palette
 import com.fabirt.podcastapp.constant.K
 import com.fabirt.podcastapp.data.service.MediaPlayerServiceConnection
 import com.fabirt.podcastapp.domain.model.Episode
@@ -53,6 +58,15 @@ class PodcastPlayerViewModel @Inject constructor(
 
     fun stopPlayback() {
         serviceConnection.transportControls.stop()
+    }
+
+    fun calculateColorPalette(drawable: Drawable, onFinised: (Color) -> Unit) {
+        val bitmap = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        Palette.from(bitmap).generate { palette ->
+            palette?.dominantSwatch?.rgb?.let { colorValue ->
+                onFinised(Color(colorValue))
+            }
+        }
     }
 
     override fun onCleared() {
